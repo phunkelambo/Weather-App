4883ea72d61cffd0e7ac14cd0b733fc8185cdd3f
phunkelambo <104282727+phunkelambo@users.noreply.github.com>
2022-06-26 11:33:54 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -117,7 +117,7 @@ function showTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  dateElement.innerHTML = formatDay(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
