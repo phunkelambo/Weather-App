c4f04e6717df13b2808e1960993300676661a599
phunkelambo <104282727+phunkelambo@users.noreply.github.com>
2022-06-26 12:45:15 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,36 +1,64 @@
-function showCurrentDate(latestDate) {
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let day = days[latestDate.getDay()];
-  let month = months[latestDate.getMonth()];
-  let date = latestDate.getDate();
-  let year = latestDate.getFullYear();
-  let hours = latestDate.getHours();
-  
+let now = new Date();
+
+let h2 = document.querySelector("h2");
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+h2.innerHTML = `📅${day} ${date} ${month} ${year}<br />
+🕑${hours}:${minutes}`;
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let minutes = latestDate.getMinutes();
+  let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = `${day}, ${date} ${month} ${year}<br /> ${hours}: ${minutes}`;
-  return dateElement
-}                         
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = date.getDay();
+  return ` ${hours}:${minutes}`;
+}
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
@@ -42,50 +70,37 @@ function formatDay(timestamp) {
 
 function displayForecast(response) {
   let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index > 0 && index < 6) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
-                    <div class="col-2">
-                <div style="min-height: 0px">
-                  <div
-                    class="collapse collapse-horizontal"
-                    id="weatherForecast"
-                  >
-                    <div class="card card-body text-center weather-forecast-box" style="width: 100%" id="forecast">
-                      <div class="weather-forecast-date">${formatDay(
-                        forecastDay.dt
-                      )}</div>
-                      <div class="weather-icon">
-                      ${forecastWeatherIcon(forecastDay.weather[0].description)}
-                      </div>
-                      <div class="weather-forecast-temperature" style="font-size: 14px">
-                        <span class="weather-forecast-temperature-max"
-                          >${Math.round(forecastDay.temp.max)}°/</span
-                        >
-                        <span class="weather-forecast-temperature-min"
-                          >${Math.round(forecastDay.temp.min)}°</span
-                        >
-                      </div>
-                      <div>
-                        <br />
-                        <i class="bi bi-umbrella"></i>
-                        <span style="font-size: 14px">${Math.round(
-                          forecastDay.pop * 100
-                        )}%</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>`;
+        <div class="col-2">
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+          
+          <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+      <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature-max">⬆️${Math.round(
+        forecastDay.temp.max
+      )}°C</span>
+      <span class="weather-forecast-temperature-min">⬇️${Math.round(
+        forecastDay.temp.min
+      )}°C</span>
+      </div>
+      </div>`;
     }
-    document.querySelector("#rain-probability").innerHTML = Math.round(
-      forecastDay.pop * 100
-    );
   });
+
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
@@ -93,7 +108,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   let apiKey = "8678fe46de622085a6470ee25e2466ff";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  
+
   axios.get(apiUrl).then(displayForecast);
 }
 
